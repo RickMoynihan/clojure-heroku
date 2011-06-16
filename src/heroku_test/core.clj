2d55266ac13465c17369ff25bc1c23ed3c4f6a4e
Rick Moynihan <rick@wgrids.com>
2011-06-16 01:00:40 +0100

    nothing but a lein project skeleton

---
+++ src/heroku_test/core.clj
@@ -0,0 +1,1 @@
+(ns heroku-test.core)
---
+++ test/heroku_test/test/core.clj
@@ -0,0 +1,6 @@
+(ns heroku-test.test.core
+  (:use [heroku-test.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
