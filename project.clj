25d96ae4b0634e6e9cd91f364c00a219e7830f9d
Rick Moynihan <rick@wgrids.com>
2011-06-16 01:34:34 +0100

    make simple ring project

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject heroku-test "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ring/ring-core "0.3.8"]
+                 [ring/ring-jetty-adapter "0.3.8"]])
